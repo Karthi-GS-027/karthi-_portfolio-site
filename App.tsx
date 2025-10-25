04da38d7ce4c664868e80754f8b534795fd66039
Karthi-GS-027 <gkarthi.ui@gmail.com>
2025-10-25 19:00:00 +0530

    feat: Embed profile picture as SVG
    Replaces the external profile picture URL with an inline SVG base64 encoded string. This improves performance by eliminating an external HTTP request and ensures the image is always available.

--- App.tsx
+++ App.tsx
@@ -12,7 +12,7 @@ const initialPortfolioData: PortfolioData = {
     'Date of Birth': '17/06/2000',
     'Gender': 'Male',
     'Marital Status': 'Single',
-    'profile_picture_url': 'https://i.imgur.com/8b23K1A.png'
+    'profile_picture_url': 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAxMDAgMTAwIj48Y2lyY2xlIGN4PSI1MCIgY3k9IjM1IiByPSIyMCIgZmlsbD0iIzRhZGU4MCIvPjxwYXRoIGQ9Ik0xNSA5NSBDIDE1IDc1LCA4NSA3NSwgODUgOTUiIGZpbGw9IiM0YWRlODAiLz48L3N2Zz4='
   },
   contact_info: {
     'Mobile': '+91-9361191640',
